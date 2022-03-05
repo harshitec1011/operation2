e564aee8e43ddb4f5e210407cb86500236265f54
s <s@DESKTOP-F64FECO>
2022-03-05 23:04:09 +0530

    my second comit

---
+++ pvariableDemo/src/pvariableDemo/pvariableDemo.java
@@ -0,0 +1,21 @@
+package pvariableDemo;
+
+public class pvariableDemo 
+{
+	public static void main(String[] args)
+	{
+		//int,float,double
+		int i=5;// 4 bytes->32 bits
+		short s=5;// 2 bytes->16 bits->-32768 to 32767
+		byte b=5;//1 byte-> 8 bit-> -128 to 127
+		long l=50000000l;//8 bytes
+		char c='A';
+		c=66;//American standard code for information interchange
+		double d1=5;//type cast ->implicit conversion
+		int k=(int)5.6;//type casting->explicit conversion
+				System.out.println(k);
+	}
+	
+	
+
+}
